61547710a15ba02ea9f91f70cfb4087d853bf458
Jamie Lopez <jmelopez@outlook.com>
2020-09-09 17:33:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,25 +1,10 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray(chocolateBars, chocolate){
-return [`${chocolate}`, ...chocolateBars]
-}
-addElementToBeginningOfArray(chocolateBars, 'butter finger');
-
-function destructivelyAddElementToBeginningOfArray(chocolateBars, chocolate) {
-return (chocolateBars.unshift(chocolate))
-}
-destructivelyAddElementToBeginningOfArray('hershey');
-
-function addElementToEndOfArray(chocolateBars, chocolate){
-return [...chocolateBars, `${chocolate}`]
-}
-addElementToEndOfArray('milky way');
-
-function destructivelyAddElementToEndOfArray(chocolateBars, chocolate) {
-return (chocolateBars.push(chocolate))
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];
 }
 
-function accessElementInArray(number){
-  return console.log(chocolateBars[number])
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array
 }
-accessElementInArray(3);

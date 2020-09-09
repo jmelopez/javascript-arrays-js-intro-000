910fb4be42cf53a17b8a2d5766c118100893ca9c
Jamie Lopez <jmelopez@outlook.com>
2020-09-09 17:35:37 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,12 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array
 }
+
+function addElementToEndOfArray(array,element){
+  retrun [...array, element];
+  }
+
+  function destructivelyAddElementToEndOfArray(array, element){
+    array.push(element);
+    return array
+  }

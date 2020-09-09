023eb05aaed053081c80653ec3160db0bd65353b
Jamie Lopez <jmelopez@outlook.com>
2020-09-09 17:10:30 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(chocolateBars, chocolate){
+return [`${chocolate}`, ...chocolateBars]
+}
+addElementToBeginningOfArray(chocolateBars, 'butter finger');
+
+function destructivelyAddElementToBeginningOfArray() {
+return (chocolateBars.unshift('hershey')
+}
+
+function addElementToEndOfArray(chocolateBars, chocolate){
+return [...chocolateBars, `${chocolate}`]
+}
+addElementToEndOfArray('milky way');
+
+function destructivelyAddElementToEndOfArray(chocolateBars, chocolate) {
+return (chocolateBars.push(chocolate))
+}
+
+function accessElementInArray()
